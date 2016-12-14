9859ed66474f95f44293123e843b420fc3ed2552
lg317 <giacomoni.l@gmail.com>
2016-12-14 18:13:25 +0000

    Removed -Werror flag and fixed <numeric> headers

--- src/simulationresults.cc
+++ src/simulationresults.cc
@@ -1,7 +1,15 @@
 #include "simulationresults.hh"
 
-template <typename ActionTree>
-SimulationRunData & SimulationResults< ActionTree >::add_run_data( const NetConfig & config, double interval )
+template <>
+SimulationRunData & SimulationResults< WhiskerTree >::add_run_data( const NetConfig & config, double interval )
+{
+
+  run_data.emplace_back( config, interval );
+  return run_data.back();
+}
+
+template <>
+SimulationRunData & SimulationResults< FinTree >::add_run_data( const NetConfig & config, double interval )
 {
 
   run_data.emplace_back( config, interval );
@@ -39,8 +47,41 @@ void SimulationResults< FinTree >::_populate_actions( SimulationResultBuffers::S
   pb.mutable_fins()->CopyFrom( actions.DNA() );
 }
 
-template <typename ActionTree>
-SimulationResultBuffers::SimulationsData SimulationResults< ActionTree >::DNA( void ) const
+
+template <>
+SimulationResultBuffers::SimulationsData SimulationResults< WhiskerTree >::DNA( void ) const
+{
+  SimulationResultBuffers::SimulationsData ret;
+
+  _populate_actions(ret);
+
+  ProblemBuffers::ProblemSettings settings;
+  settings.set_prng_seed( prng_seed );
+  settings.set_tick_count( tick_count );
+  ret.mutable_settings()->CopyFrom( settings );
+
+  for ( const auto &run : run_data ) {
+    SimulationResultBuffers::SimulationRunData * run_data_pb = ret.add_run_data();
+    run_data_pb->set_log_interval_ticks( run.interval() );
+    run_data_pb->mutable_config()->CopyFrom( run.config().DNA() );
+
+    for ( const auto &datum : run.data() ) {
+      SimulationResultBuffers::SimulationRunDataPoint * data_pb = run_data_pb->add_point();
+      data_pb->set_seconds( datum.seconds() );
+
+      for (const auto &sender_datum : datum.sender_data() ) {
+        SimulationResultBuffers::SenderDataPoint * sender_data_pb = data_pb->add_sender_data();
+        sender_data_pb->CopyFrom( sender_datum.DNA() );
+      }
+    }
+  }
+
+  return ret;
+}
+
+
+template <>
+SimulationResultBuffers::SimulationsData SimulationResults< FinTree >::DNA( void ) const
 {
   SimulationResultBuffers::SimulationsData ret;
 
